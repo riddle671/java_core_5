16a041b1d5eb129b367a29fda02cf85041e00f84
riddle671@yandex.ru <12345Xxx2>
2022-11-19 11:19:36 +0300

    home 19.11.22

--- src/homework1.java
+++ src/homework1.java
@@ -18,7 +18,6 @@ public class homework1 {
             System.out.println("Ребенок идет в университет");
         }
         // Задание №3
-        // Задание №3
         boolean chicken = true;
         boolean vegetables = false;
         boolean sour = true;
